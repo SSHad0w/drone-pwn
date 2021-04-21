85de0acdc324af866bcded9729890cffa74a3792
SSHad0w <sshad0w@localhost.localdomain>
2021-04-21 00:51:08 -0400

    init commit

---
+++ fly_script.py
@@ -0,0 +1,54 @@
+import socket
+import time
+
+
+
+tello = ('192.168.10.1', 8889)
+ 
+def init_drone():
+ 
+    # create upd client on PC
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error as err:
+        print(err)
+        exit()
+ 
+    try:
+        # send control commands to the drone
+        s.sendto(b'command', tello)
+        time.sleep(5)
+    except socket.error as err:
+        print(err)
+ 
+    return s
+ 
+def takeoff(s):
+    s.sendto(b'takeoff', tello)
+    time.sleep(10)
+ 
+def rotate90(s):
+    s.sendto(b'cw 90', tello)
+    time.sleep(10)
+ 
+def flip_left(s):
+    s.sendto(b'flip l', tello)
+    time.sleep(10)
+ 
+def land(s):
+    s.sendto(b'land', tello)
+    time.sleep(5)
+ 
+def main():
+    s = init_drone()
+ 
+    takeoff(s)
+ 
+    rotate90(s)
+ 
+    flip_left(s)
+ 
+    land(s)
+ 
+if __name__ == '__main__':
+    main()
