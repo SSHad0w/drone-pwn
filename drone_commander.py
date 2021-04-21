207c756904d9d75f5659e13a2e462fac2918a1b1
Joshua Adkins <joshadkins@Joshuas-MacBook-Pro.local>
2021-04-21 02:03:55 -0400

    updated drone commander to add help command

--- drone_commander.py
+++ drone_commander.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 TELLO = ('192.168.10.1', 8889)
-SLEEP_TIME = 3
+SLEEP_TIME = 4.5
 
 def init_drone():
     print('==> Connecting to Drone...')
@@ -25,6 +25,25 @@ def init_drone():
     print('==> Connection Established')
     return s
 
+def help():
+
+    print('''==> commands: 
+        1) takeoff
+        2) land 
+        3) streamon
+        4) streamoff
+        5) emergency*
+        6) up x(cm)*
+        7) down xc(m)* 
+        8) left x(cm)*
+        9) right x(cm)*
+        10) forward x(cm)*
+        11) back x*
+        12) cw x(degress) - rotate clockwise
+        13) ccw x(degrees) - rotate counter clockwise
+        14) flip x(l,r,f,b)
+
+        * - use with caution''')
 
 def main():
     # Establish socket connection
@@ -32,17 +51,19 @@ def main():
 
     # Main Loop
     while True:
+
         cmd = input('D R O N E > ')
         if cmd == '' or cmd == 'exit':
             break
+        elif cmd =='help':
+            help()
         else:
             skt.sendto(cmd.encode('ascii'), TELLO)
             time.sleep(SLEEP_TIME)
             print('<== Sent')
  
-    print('==> Exiting and Landing...')
-    time.sleep(SLEEP_TIME)
-    skt.sendto(b'land', TELLO)
+    print('==> Exiting ...')
+    
  
 if __name__ == '__main__':
     main()
