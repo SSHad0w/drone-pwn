6b153ed551e731323c12ecbc1a0728dfa78c713e
SSHad0w <60116694+SSHAD0w@users.noreply.github.com>
2022-03-15 10:36:58 -0400

    Update py_print_parser.py

--- py-parser/py_print_parser.py
+++ py-parser/py_print_parser.py
@@ -1,16 +1,21 @@
-path = './fingerprint2.txt'
-days_file = open(path,'r')
-days = days_file.read().replace('\nSF:','').replace('%','\n')
+# nmap unknown fingerprint parser
+# SSHad0w
+# Version 1.0
 
-new_path = path + '.txt'
-new_days = open(new_path,'w')
+path = './fingerprint2.txt' # Input file
+scan_file = open(path,'r')
+# Replaces all instances of "SF:" with nothing and "% with newline"
+scan = scan_file.read().replace('\nSF:','').replace('%','\n') 
 
-title = 'NEW FILE:\n\n'
-new_days.write(title)
+new_path = path + '.txt' # Renames file to filename.txt.txt
+new_scan = open(new_path,'w')
+
+title = 'NEW FILE:\n\n' # Prints "NEW FILE" in the new text file
+new_scan.write(title)
 print(title)
 
-new_days.write(days)
-print(days)
+new_scan.write(scan) #
+print(scan)
 
-days_file.close()
-new_days.close()
+scan_file.close()
+new_scan.close()
