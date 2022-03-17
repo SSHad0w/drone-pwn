54b6557a2a92b646ab8f5be79c78b21cbd2f8d97
SSHad0w <SSHad0w@protonmail.com>
2022-03-17 14:02:04 -0400

    Updating the parser with new features.

--- py-parser/py_print_parser.py
+++ py-parser/py_print_parser.py
@@ -6,28 +6,42 @@ import argparse
 # I need a cooler name. NUSVFP FUNSVP 
 parser = argparse.ArgumentParser(description="Nmap Unknown Service Version Fingerprint Parser")
 parser.add_argument("-helpme", "--helpme", type=str, metavar="h", required=False, help="Help Menu")
-parser.add_argument("-f","--file", type=str, dest="file", required=True, nargs='*', help="Input filename")
-parser.add_argument("-o","--out", type=str, dest="decode", required=False, help="Output file")
+parser.add_argument("-f","--file", type=str, dest="file", required=True, help="Input filename") #"One of the more common uses of nargs='?' is to allow optional input and output files"
+parser.add_argument("-o","--out", type=str, dest="output", required=False, help="Output file")
 args = parser.parse_args()
 
 
+def parse_input(): #Takes input file
+	global path
+	path = args.file # Input file
+	scan_file = open(path,'r')
+	# Replaces all instances of "SF:" with nothing and "% with newline"
+	parsed = scan_file.read().replace('\nSF:','').replace('%','\n') 
+	return parsed
 
-def input_file() # Takes input file
-path = args.file # Input file
+def rename_file(new_name):
+	output = open(new_name,'w')
+	title = 'NEW FILE:\n\n' # Prints "NEW FILE" in the new text file
+	output.write(title)
+	print(title)
 
-scan_file = open(path,'r')
-# Replaces all instances of "SF:" with nothing and "% with newline"
-scan = scan_file.read().replace('\nSF:','').replace('%','\n') 
+	output.write(parse_input()) #
+	print("File Written")
+	output.close()
 
-new_path = path + '.txt' # Renames file to filename.txt.txt
-new_scan = open(new_path,'w')
 
-title = 'NEW FILE:\n\n' # Prints "NEW FILE" in the new text file
-new_scan.write(title)
-print(title)
+def print_text():
+	title = 'NEW FILE:\n\n' # Prints "NEW FILE" in the new text file
+	print(title)
+	print(parse_input())
 
-new_scan.write(scan) #
-print(scan)
+def main():
+	parse_input()
 
-scan_file.close()
-new_scan.close()
+	if args.output:
+		rename_file(args.output)
+	else:
+		print_text()
+
+if __name__ == '__main__':
+	main()
